b5327b34b36fe26851c8fe4878c04b94b21d4cb4
Rodrigo Krug <rodrigo.krug1@gmail.com>
2022-12-29 18:51:02 -0300

    Módulo cliente

---
+++ src/services/NSE.Cliente.API/Application/Events/ClienteRegistradoEvent.cs
@@ -0,0 +1,21 @@
+﻿using NSE.Core.Messages;
+using System;
+
+namespace NSE.Clientes.API.Application.Events
+{
+    public class ClienteRegistradoEvent : Event
+    {
+        public ClienteRegistradoEvent(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Controllers/ClientesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Clientes.API.Application.Commands;
+using NSE.Core.Mediator;
+using NSE.WebApi.Core.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Controllers
+{
+    public class ClientesController : MainController
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public ClientesController(IMediatorHandler mediatorHandler)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        [HttpGet("clientes")]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _mediatorHandler.EnviarComando(
+                new RegistrarClienteCommand(Guid.NewGuid(), "Rodrigo Krug", "rodrigo.krug1@gmail.com", "83746900034"));
+
+            return CustomResponse(result);
+        }
+    }
+}
